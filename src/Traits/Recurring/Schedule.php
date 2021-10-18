b3f135cd5d1945fb9ffbdbe836056e9d504f04b5
Gilbert Rutatiina <rutatiina@ruginem.org>
2021-10-18 11:51:25 +0300

    Bug fixing on creation and updating of items

--- src/Traits/Recurring/Schedule.php
+++ src/Traits/Recurring/Schedule.php
@@ -34,6 +34,8 @@ trait Schedule
             return false;
         }
 
+        if (!Schema::hasTable((new RecurringExpenseProperty)->getTable())) return false;
+
         //$schedule->call(function () {
         //    Log::info('recurringInvoiceSchedule via trait has been called #updated');
         //})->everyMinute()->runInBackground();
